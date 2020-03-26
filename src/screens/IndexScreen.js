5172a15d36b3c6937b90ecd53417962a8dcc217b
oliviermtl <olivierguyou@msn.com>
2020-03-26 08:04:57 +0100

    added basic styling

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,5 +1,5 @@
 import React, {useContext} from 'react';
-import {Text, FlatList, Button} from 'react-native';
+import {Text, FlatList, Button, View, StyleSheet} from 'react-native';
 import {Context} from '../context/BlogContext';
 
 const IndexScreen = () => {
@@ -7,17 +7,38 @@ const IndexScreen = () => {
 
   return (
     <>
-      <Text>IndexScreen</Text>
       <Button title="Add Post" onPress={addBlogPost} />
       <FlatList
         data={state}
         keyExtractor={blogPost => blogPost.title}
         renderItem={({item}) => {
-          return <Text>{item.title}</Text>;
+          return (
+            <View style={styles.row}>
+              <Text style={styles.title}>{item.title}</Text>
+              <Text style={styles.delete}>X</Text>
+            </View>
+          );
         }}
       />
     </>
   );
 };
 
+const styles = StyleSheet.create({
+  delete: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginHorizontal: 8,
+    paddingVertical: 10,
+
+    borderBottomWidth: 1,
+  },
+  title: {
+    fontSize: 18,
+  },
+});
 export default IndexScreen;
